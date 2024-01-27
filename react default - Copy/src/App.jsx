90af687cbc8d41ad00f0b960cbb7a9d0390c052f
ieeeza <cesardermer1@gmail.com>
2024-01-26 21:59:08 -0300

    26/01/2024

---
+++ react default - Copy/src/App.jsx
@@ -0,0 +1,15 @@
+// click event = An interaction when a user clicks on a specific element. We can respond to clicks by passing a callback 
+//               to the onClick event handler
+
+import Button from "./Button"
+
+function App() {
+
+  return (
+    <>
+      <Button/>
+    </>
+  )
+}
+
+export default App
